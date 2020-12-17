b5e2309252ed6f92e535798dc36d2eb141579ca9
Seth <spawn@wisc.edu>
2020-12-16 23:32:10 -0600

    added fill_holes()

--- findDrainageOutlets.R
+++ findDrainageOutlets.R
@@ -6,7 +6,8 @@ rm(list = ls())
 packages = c(
   "sf",
   "nhdR",
-  "nhdplusTools"
+  "nhdplusTools",
+  "smoothr"
 )
 
 install.packages(setdiff(packages, rownames(installed.packages()))) 
@@ -18,6 +19,9 @@ findDrainageOutlets = function(poly){
   
   # explicitly cast poly as a polygon
   poly = st_cast(poly, "POLYGON")
+  
+  # fill any internal holes in poly (throw errors when calculating intersection)
+  poly = fill_holes(poly, threshold = st_area(poly))
   
   # load vpus for querying nhd
   vpus = nhdR::vpu_shp
@@ -74,34 +78,34 @@ findDrainageOutlets = function(poly){
     
   })
   
-  return(outlet_comids)
+  return(unlist(outlet_comids))
   
 }
 
 #================================================================================================================
 
-# load critical habitat polys
-file = 'FCH_Notropis_topeka_20040727.zip'
-
-# unzip a shapefile to 'temp_unzip' subdirectory
-unzip(file.path('crithab_all_shapefiles', file), exdir = file.path('crithab_all_shapefiles', 'unzipped'))
-
-# read unzipped shapefile
-shp = try(st_read(file.path('crithab_all_shapefiles', 'unzipped', gsub('.zip', '.shp', file))))
-
-# dissolve adjoining polygons
-shp = st_as_sf(st_cast(st_union(shp), "POLYGON"))
-
-outlets = lapply(split(shp, 1:nrow(shp)), findDrainageOutlets)
+# # load critical habitat polys
+# file = 'FCH_Notropis_topeka_20040727.zip'
+# 
+# # unzip a shapefile to 'temp_unzip' subdirectory
+# unzip(file.path('crithab_all_shapefiles', file), exdir = file.path('crithab_all_shapefiles', 'unzipped'))
+# 
+# # read unzipped shapefile
+# shp = try(st_read(file.path('crithab_all_shapefiles', 'unzipped', gsub('.zip', '.shp', file))))
+# 
+# # dissolve adjoining polygons
+# shp = st_as_sf(st_cast(st_union(shp), "POLYGON"))
+# 
+# outlets = lapply(split(shp, 1:nrow(shp)), findDrainageOutlets)
 
 #================================================================================================================
 
-require(ggplot2)
-ggplot()+
-  #geom_sf(data = poly_vpus)+
-  geom_sf(data = poly, fill = 'red')+
-  geom_sf(data = vpu_flowlines_clipped)+
-  geom_sf(data = pts, aes(colour = outlet_comid))+
-  geom_sf(data = flowlines, color = 'green')+
-  geom_sf(data = pts[which(is.na(pts$outlet_comid)),], colour = 'green', size = 3)
+# require(ggplot2)
+# ggplot()+
+#   #geom_sf(data = poly_vpus)+
+#   geom_sf(data = poly, fill = 'red')+
+#   geom_sf(data = vpu_flowlines_clipped)+
+#   geom_sf(data = pts, aes(colour = outlet_comid))+
+#   geom_sf(data = flowlines, color = 'green')+
+#   geom_sf(data = pts[which(is.na(pts$outlet_comid)),], colour = 'green', size = 3)
 
